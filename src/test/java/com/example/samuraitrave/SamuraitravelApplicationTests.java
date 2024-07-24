90ea0ddf56f2b31deac38badf94aa44a42806051
parienigh <8suta.to10621@gmail.com>
2024-07-24 21:39:43 +0900

    second commit

---
+++ src/test/java/com/example/samuraitrave/SamuraitravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.samuraitrave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamuraitravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
